6169fb54b5baa360c027fc4f9d8b0f549816bad3
C <charlienice123@gmail.com>
2020-10-12 09:31:17 +0100

    factorial example

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int start = 5;
+        System.out.println(factorial(start));
+    }
+
+    public static int factorial(int fac){
+        if(fac==0){
+            return 1;
+        }
+        return fac * factorial(fac-1);
+    }
+}
